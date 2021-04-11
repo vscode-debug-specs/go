56798ac09c8d5a273805bb341dbfa4e4583fc285
Atsushi Morimoto (74th) <site@74th.tech>
2021-04-11 06:37:19 +0000

    add grpc

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -2,6 +2,8 @@
 protoc \
     --go_out=. \
     --go_opt=paths=source_relative \
+    "--go_opt=Mgateways/grpc/service.proto=github.com/vscode-debug-specs/go/gateways/grpc;grpc" \
     --go-grpc_out=. \
     --go-grpc_opt=paths=source_relative \
-    proto/service.proto
+    "--go-grpc_opt=Mgateways/grpc/service.proto=github.com/vscode-debug-specs/go/gateways/grpc;grpc" \
+    gateways/grpc/service.proto
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/vscode-debug-specs/go
 
 go 1.14
+
+require (
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
